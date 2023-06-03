f262fdfc77022f780b343f3326f85ce2bdad5852
stardvst <lilia.manukyan@hotmail.com>
2023-06-03 20:46:07 +0400

    deleting a hero

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -45,6 +45,14 @@ export class HeroService {
     );
   }
 
+  deleteHero(id: number): Observable<Hero> {
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.delete<Hero>(url, this.httpOptions).pipe(
+      tap(_ => this.log(`deleted hero id=${id}`)),
+      catchError(this.handleError<Hero>('deleteHero'))
+    );
+  }
+
   private log(message: string) {
     this.messageService.add(`HeroService: ${message}`);
   }
